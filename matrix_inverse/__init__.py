0f80ca04d8f94f6b7ee69dbf7b418b5b4be1c93e
Peter Knipp <peteraknipp@gmail.com>
2024-11-12 19:26:19 -0500

    Tighten code.

--- matrix_inverse/__init__.py
+++ matrix_inverse/__init__.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 top = "<head><title>Matrix inverse</title></head><body>"
 bottom = "<p align=center>creator:&nbsp;<a href='https://pknipp.github.io/' target='_blank' rel='noopener noreferrer'>Peter Knipp</a><br/>repo:&nbsp;<a href='https://github.com/pknipp/matrix-inverse' target='_blank'  rel='noopener noreferrer'>github.com/pknipp/matrix-inverse</a></p></body>"
 
-
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def react_root(path):
@@ -24,22 +23,36 @@ def hello():
 @app.route('/<square_in>')
 def square(square_in):
     results = helper.parse(False, square_in)
-    # if isinstance(results, str):
-        # return top + results + '</body>'
+    determinant = results["determinant"]
+    n = len(results["original matrix"])
+    m = len(results["inhomogeneous part"])
+    inhomogeneous_part=results["inhomogeneous part"]
+    original_matrix = enumerate(results["original matrix"])
+    solutions = results["solutions"]
+    inverse_matrix = results.get("inverse matrix")
+    if inverse_matrix is not None:
+        inverse_matrix = enumerate(inverse_matrix)
+
     if "error" in results:
         return render_template('error.html', message=results["error"]["message"], strings=results["error"]["strings"])
     else:
-        return render_template("results.html", determinant=results["determinant"], n=len(results["original matrix"]), m=len(results["inhomogeneous part"]),inhomogeneous_part=results["inhomogeneous part"], original_matrix=enumerate(results["original matrix"]), solutions=results["solutions"], inverse_matrix=enumerate(results["inverse matrix"]))
+        return render_template("results.html", determinant=determinant, n=len(original_matrix), m=len(inhomogeneous_part),inhomogeneous_part=inhomogeneous_part, original_matrix=original_matrix, solutions=solutions, inverse_matrix=inverse_matrix)
 
 @app.route('/<square_in>/<rect_in>')
 def rect(square_in, rect_in):
     results = helper.parse(False, square_in, rect_in)
-    # if isinstance(results, str):
-        # return top + results + "</body>"
+    results = helper.parse(False, square_in)
+    determinant = results["determinant"]
+    n = len(results["original matrix"])
+    m = len(results["inhomogeneous part"])
+    inhomogeneous_part=results["inhomogeneous part"]
+    original_matrix = enumerate(results["original matrix"])
+    solutions = results["solutions"]
+    inverse_matrix = results.get("inverse matrix")
     if "error" in results:
         return render_template('error.html', message=results["error"]["message"], strings=results["error"]["strings"])
     else:
-        return render_template("results.html", determinant=results["determinant"], n=len(results["original matrix"]), m=len(results["inhomogeneous part"]), inhomogeneous_part=results["inhomogeneous part"], original_matrix=enumerate(results["original matrix"]), solutions=results["solutions"], inverse_matrix=enumerate(results["inverse matrix"]))
+        return render_template("results.html", determinant=determinant, n=len(original_matrix), m=len(inhomogeneous_part),inhomogeneous_part=inhomogeneous_part, original_matrix=original_matrix, solutions=solutions, inverse_matrix=inverse_matrix)
 
 @app.route('/json/<square_in>')
 def json_square(square_in):
